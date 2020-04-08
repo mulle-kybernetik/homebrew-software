94371865b476200ee061196a8268647cfa971191
Nat! <nat-tv@mulle-kybernetik.com>
2020-04-08 14:51:14 +0200

    0.13.0 0.13.0 of mulle-make

--- mulle-make.rb
+++ mulle-make.rb
@@ -1,9 +1,9 @@
 class MulleMake < Formula
 desc "💄 Build projects with tools like cmake, meson, autoconf"
 homepage "https://github.com/mulle-sde/mulle-make"
-url "https://github.com/mulle-sde/mulle-make/archive/0.11.0.tar.gz"
-sha256 "6756728fd96956dff3b57aa214c63e0f2a2573f9a3379b2de254e300404946ff"
-# version "0.11.0"
+url "https://github.com/mulle-sde/mulle-make/archive/0.13.0.tar.gz"
+sha256 "d90db42ad8ea699165f4582701a9eb2f8ff366d9bb128a27598c82cef3203618"
+# version "0.13.0"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
