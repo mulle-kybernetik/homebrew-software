ce38c0b89558bc379c8ca41f50f72fcafcf794ad
Nat! <nat@mulle-kybernetik.com>
2020-04-19 18:35:16 +0200

    0.1.0 0.1.0 of mulle-web-developer

--- mulle-web-developer.rb
+++ mulle-web-developer.rb
@@ -2,7 +2,7 @@ class MulleWebDeveloper < Formula
 desc "🎒 MulleWeb developer kit for mulle-sde"
 homepage "https://github.com/MulleWeb/mulle-web-developer"
 url "https://github.com/MulleWeb/mulle-web-developer/archive/0.1.0.tar.gz"
-sha256 "dd100af298fcc8bde6045c2cba6ca981e1444aba516af63a98aec0d4b28dc5dd"
+sha256 "2a13723af66f837dcfc43194854fe891e16bbf7cafd885a406da0e69c4b619b2"
 # version "0.1.0"
 
 depends_on "mulle-kybernetik/software/foundation-developer"
