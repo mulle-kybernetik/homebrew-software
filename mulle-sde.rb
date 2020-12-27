4f88f5793a9b27c898c3816e89f27977b339d2c4
Nat! <nat@mulle-kybernetik.com>
2020-12-27 16:10:00 +0100

    0.45.0 0.45.0 of mulle-sde

--- mulle-sde.rb
+++ mulle-sde.rb
@@ -1,9 +1,9 @@
 class MulleSde < Formula
 desc "💠 Cross-platform IDE for the command-line"
 homepage "https://github.com/mulle-sde/mulle-sde"
-url "https://github.com/mulle-sde/mulle-sde/archive/0.43.2.tar.gz"
-sha256 "f6b0591791d67ddc4f011ccd0a074ffd248ed580d1748ce2eb000b202f010cfe"
-# version "0.43.2"
+url "https://github.com/mulle-sde/mulle-sde/archive/0.45.0.tar.gz"
+sha256 "415eb100d804f24022e0373d0a9e47c3d0672a7790ad42d009193f32558c057a"
+# version "0.45.0"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 depends_on "mulle-kybernetik/software/mulle-env"
@@ -11,6 +11,7 @@ depends_on "mulle-kybernetik/software/mulle-craft"
 depends_on "mulle-kybernetik/software/mulle-monitor"
 depends_on "mulle-kybernetik/software/mulle-platform"
 depends_on "mulle-kybernetik/software/mulle-sourcetree"
+depends_on "mulle-kybernetik/software/mulle-template"
 depends_on "mulle-kybernetik/software/mulle-test"
 def install
   system "./bin/installer", "#{prefix}"
