5335bfd4bf9700f3141154546e50b35063dad0b1
Nat! <nat@mulle-kybernetik.com>
2022-01-12 12:14:44 +0100

    0.23.0 0.23.0 of mulle-sde-developer

--- mulle-sde-developer.rb
+++ mulle-sde-developer.rb
@@ -1,9 +1,9 @@
 class MulleSdeDeveloper < Formula
 desc "🎓 mulle-sde installer + various extensions for mulle-sde"
 homepage "https://github.com/mulle-sde/mulle-sde-developer"
-url "https://github.com/mulle-sde/mulle-sde-developer/archive/0.22.0.tar.gz"
-sha256 "881340f0598a2647cf59e5f0cbff5d7adc7245e03d2263ffaa889f2db5f1b153"
-# version "0.22.0"
+url "https://github.com/mulle-sde/mulle-sde-developer/archive/0.23.0.tar.gz"
+sha256 "c93ee835794e21ef2d149dca9c5fb02a83a15b77babfab62c9cbb22604a50b27"
+# version "0.23.0"
 
 depends_on "mulle-kybernetik/software/mulle-sde"
 depends_on "cmake"
