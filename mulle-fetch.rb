f496e0af24be727ece7bf11151627867d0b6d889
Nat! <nat@mulle-kybernetik.com>
2022-01-12 12:03:00 +0100

    3.0.0 3.0.0 of mulle-fetch

--- mulle-fetch.rb
+++ mulle-fetch.rb
@@ -1,9 +1,9 @@
 class MulleFetch < Formula
 desc "🏃🏿 Download and unpack source repositories or archives"
 homepage "https://github.com/mulle-sde/mulle-fetch"
-url "https://github.com/mulle-sde/mulle-fetch/archive/2.0.0.tar.gz"
-sha256 "3fc92b6a4eeff5706387a5851cb940b386a4a0798016d3e0647b17633bdef429"
-# version "2.0.0"
+url "https://github.com/mulle-sde/mulle-fetch/archive/3.0.0.tar.gz"
+sha256 "9ac0cfee005f67c4884f93b2b91f2c6c0087edec2206671bd3e76a5e730e6cfb"
+# version "3.0.0"
 
 depends_on "mulle-kybernetik/software/mulle-domain"
 def install
