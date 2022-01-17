5704b32da530930e55f55ff33b0c1bd6d17dd807
Nat! <nat@mulle-kybernetik.com>
2022-01-17 14:45:59 +0100

    Update Foundation.rb
    Fix Foundation formula url

--- Foundation.rb
+++ Foundation.rb
@@ -5,14 +5,16 @@ url "https://github.com/MulleFoundation/Foundation/archive/0.20.0.tar.gz"
 sha256 "42080b41c2a2723cc7b1a996cdce50c2ba6117f5ae4b4bc794780f9925289fc0"
 # version "0.20.0"
 
-depends_on "codeon-gmbh/software/mulle-clang"
+depends_on "mulle-objc/software/mulle-clang"
 
 depends_on "mulle-kybernetik/software/mulle-sde" => :build
 depends_on "mulle-kybernetik/software/mulle-craft" => :build
 depends_on "mulle-kybernetik/software/mulle-dispense" => :build
+depends_on "mulle-kybernetik/software/mulle-domain" => :build
 depends_on "mulle-kybernetik/software/mulle-fetch" => :build
 depends_on "mulle-kybernetik/software/mulle-make" => :build
 depends_on "mulle-kybernetik/software/mulle-platform" => :build
+depends_on "mulle-kybernetik/software/mulle-semver" => :build
 depends_on "mulle-kybernetik/software/mulle-sourcetree" => :build
 depends_on "mulle-kybernetik/software/mulle-bashfunctions" => :build
 depends_on "cmake" => :build
