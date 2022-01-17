26e7f7cd6dd2c38334c5e2c59b2813dfa93b0556
Nat! <nat@mulle-kybernetik.com>
2022-01-17 13:53:31 +0100

    Update mulle-project.rb
    Fix URLs

--- mulle-project.rb
+++ mulle-project.rb
@@ -1,7 +1,7 @@
 class MulleProject < Formula
 desc "🤷🏾‍♀️ Manage project versions and releases"
-homepage "https://github.com/mulle-sde/mulle-project"
-url "https://github.com/mulle-sde/mulle-project/archive/3.0.0.tar.gz"
+homepage "https://github.com/mulle-nat/mulle-project"
+url "https://github.com/mulle-nat/mulle-project/archive/3.0.0.tar.gz"
 sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 # version "3.0.0"
 
