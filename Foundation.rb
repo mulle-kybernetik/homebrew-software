c19fd07137c3fb1f5f7875891e65150549ad6c85
Nat! <nat@mulle-kybernetik.com>
2022-07-04 18:56:21 +0200

    0.21.0 0.21.0 of Foundation

--- Foundation.rb
+++ Foundation.rb
@@ -1,20 +1,18 @@
 class Foundation < Formula
 desc "💍 Convenient wrapper for the MulleFoundation"
 homepage "https://github.com/MulleFoundation/Foundation"
-url "https://github.com/MulleFoundation/Foundation/archive/0.20.0.tar.gz"
-sha256 "f338c2c621fb0355c5257de7f365eb28e9557e28022b7d2d30d598dec44a8058"
-# version "0.20.0"
+url "https://github.com/MulleFoundation/Foundation/archive/0.21.0.tar.gz"
+sha256 "3432ee6141104aaa2a75b5a045b81a7edf07bf5e4a5f9e50f097cf10c534a730"
+# version "0.21.0"
 
-depends_on "mulle-objc/software/mulle-clang-project"
+depends_on "codeon-gmbh/software/mulle-clang"
 
 depends_on "mulle-kybernetik/software/mulle-sde" => :build
 depends_on "mulle-kybernetik/software/mulle-craft" => :build
 depends_on "mulle-kybernetik/software/mulle-dispense" => :build
-depends_on "mulle-kybernetik/software/mulle-domain" => :build
 depends_on "mulle-kybernetik/software/mulle-fetch" => :build
 depends_on "mulle-kybernetik/software/mulle-make" => :build
 depends_on "mulle-kybernetik/software/mulle-platform" => :build
-depends_on "mulle-kybernetik/software/mulle-semver" => :build
 depends_on "mulle-kybernetik/software/mulle-sourcetree" => :build
 depends_on "mulle-kybernetik/software/mulle-bashfunctions" => :build
 depends_on "cmake" => :build
