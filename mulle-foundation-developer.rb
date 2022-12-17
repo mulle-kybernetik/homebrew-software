8e6fa188bceb09c44d4383889b3c48a65f058eaf
Nat! <nat@mulle-kybernetik.com>
2022-12-17 21:35:04 +0100

    0.22.0 0.22.0 of mulle-foundation-developer

--- mulle-foundation-developer.rb
+++ mulle-foundation-developer.rb
@@ -1,9 +1,9 @@
 class MulleFoundationDeveloper < Formula
 desc "👑 Objective-C development with the MulleFoundation and mulle-sde"
 homepage "https://github.com/MulleFoundation/mulle-foundation-developer"
-url "https://github.com/MulleFoundation/mulle-foundation-developer/archive/0.21.0.tar.gz"
-sha256 "17373211b3575df27a1160b1fee89ca4fcadc93d61645bcabfbf7273f70aabe3"
-# version "0.21.0"
+url "https://github.com/MulleFoundation/mulle-foundation-developer/archive/0.22.0.tar.gz"
+sha256 "aeac1340fa1018e97d600284adcbfa42e2b57b600eb30c46129bfd9272ed8478"
+# version "0.22.0"
 
 depends_on "mulle-kybernetik/software/mulle-objc-developer"
 def install
