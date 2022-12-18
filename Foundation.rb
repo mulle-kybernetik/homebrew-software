9c9e05485429179144bb78b7a7691756ecbcb08a
Nat! <nat@mulle-kybernetik.com>
2022-12-18 22:52:54 +0100

    0.21.1 0.21.1 of Foundation

--- Foundation.rb
+++ Foundation.rb
@@ -2,17 +2,17 @@ class Foundation < Formula
 desc "💍 Convenient wrapper for the MulleFoundation"
 homepage "https://github.com/MulleFoundation/Foundation"
 url "https://github.com/MulleFoundation/Foundation/archive/0.21.1.tar.gz"
-sha256 "f1fdb3f0de108033e02014eb306fa5c03b977ef6a4cf47649ba382e1473b55b5"
+sha256 "8aef23511ea692f3f1180f348524f2d5992390e6370ed2cc423758d23c7cb7f3"
 # version "0.21.1"
 
-depends_on "mulle-objc/software/mulle-clang"
+depends_on "mulle-objc/software/mulle-clang-project"
 depends_on "mulle-kybernetik/software/mulle-sde"
 depends_on "cmake"
 depends_on "ninja"
 
   def install
     system "mulle-sde", "clean", "cache"
-    system "mulle-sde","install","--standalone","--prefix","#{prefix}","https://github.com/MulleFoundation/Foundation/archive/refs/tags/latest.tar.gz"
+    system "mulle-sde","install","--standalone","--prefix","#{prefix}","#{url}"
   end
 end
 # FORMULA Foundation.rb
