e0cd52a4f5bb0b2c3693a1b2c5a0e1a4b2aa59ff
Nat! <nat@mulle-kybernetik.com>
2022-12-20 00:07:03 +0100

    0.9.1 0.9.1 of mulle-xcode-to-cmake

--- mulle-xcode-to-cmake.rb
+++ mulle-xcode-to-cmake.rb
@@ -1,19 +1,11 @@
 class MulleXcodeToCmake < Formula
 desc "🎭 Convert Xcode projects to cmake CMakeLists.txt "
 homepage "https://github.com/mulle-nat/mulle-xcode-to-cmake"
-url "https://github.com/mulle-nat/mulle-xcode-to-cmake/archive/18.48.6.tar.gz"
-sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
-# version "18.48.6"
-  depends_on :xcode => :build
-  depends_on :macos
-
-  def install
-    system "xcodebuild", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "install"
-  end
-
-  test do
-    system "#{bin}/mulle-genstrings", "--version"
-  end
-
+url "https://github.com/mulle-nat/mulle-xcode-to-cmake/archive/0.9.1.tar.gz"
+sha256 "198591176507facdb51126a5acda0ca1babbd7278e84df17abb3f6dcf2e4bba5"
+# version "0.9.1"
+   def install
+      system "xcodebuild", "DSTROOT=#{prefix}", "INSTALL_PATH=/bin", "install"
+   end
 end
 # FORMULA mulle-xcode-to-cmake.rb
