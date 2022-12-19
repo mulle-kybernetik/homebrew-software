dc84d2a9c3c12f8377c6e3bef6a29311e6747164
Nat! <nat@mulle-kybernetik.com>
2022-12-19 16:03:36 +0100

    1.2.8 1.2.8 of mulle-xcode-settings

--- mulle-xcode-settings.rb
+++ mulle-xcode-settings.rb
@@ -2,13 +2,15 @@ class MulleXcodeSettings < Formula
 desc "🗿 Edit Xcode build settings from the command line"
 homepage "https://github.com/mulle-nat/mulle-xcode-settings"
 url "https://github.com/mulle-nat/mulle-xcode-settings/archive/1.2.8.tar.gz"
-sha256 "b56cc6c5228a0fb0211a1ab61018bcaf1860047111d6fdddbd7b7fdd1e8b52a4"
+sha256 "2e576475112dbdf2b86907827bc5c908f815e092101ed56ecf167c03b5a9be8a"
 # version "1.2.8"
 
 depends_on "mulle-kybernetik/software/mulle-foundation-developer" => :build
 depends_on "cmake" => :build
+depends_on "mulle-kybernetik/software/mulle-make" => :build
+
 def install
-  system "./bin/installer", "#{prefix}"
+system "mulle-make", "-v", "install", "--prefix", "#{prefix}", "."
 end
 end
 # FORMULA mulle-xcode-settings.rb
