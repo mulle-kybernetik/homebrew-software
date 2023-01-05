b74a034e7b84ced69e98ddfa11111591561e762a
Nat! <nat@mulle-kybernetik.com>
2023-01-05 01:01:45 +0100

    fix hash

--- mulle-objc-developer.rb
+++ mulle-objc-developer.rb
@@ -2,7 +2,7 @@ class MulleObjcDeveloper < Formula
 desc "🎩 mulle-objc developer kit for mulle-sde"
 homepage "https://github.com/mulle-objc/mulle-objc-developer"
 url "https://github.com/mulle-objc/mulle-objc-developer/archive/0.24.0.tar.gz"
-sha256 "398af9b507f26e98121e4bb599029be63eff45dea42be0ac7d2c631e101d94bf"
+sha256 "b85a66d47612f95fea4ec6e00c5e511bfbcf7e45fc90fb0774cf92591da3efaa"
 # version "0.24.0"
 
 depends_on "mulle-kybernetik/software/mulle-clang-project"
