a4af2270fee938d82396e06e0a0f015c3966f1aa
Nat! <nat@mulle-kybernetik.com>
2023-01-13 20:21:18 +0100

    1859.1.3 1859.1.3 of mulle-scion

--- mulle-scion.rb
+++ mulle-scion.rb
@@ -1,17 +1,16 @@
 class MulleScion < Formula
 desc "A modern template engine for Objective C"
 homepage "https://github.com/mulle-nat/MulleScion"
-url "https://github.com/mulle-nat/MulleScion/archive/1859.1.2.tar.gz"
-sha256 "90d68388d44720ba642d86c38e89a307c0cb5ecfbff8ae0e8382324b27ee0c86"
-# version "1859.1.2"
+url "https://github.com/mulle-nat/MulleScion/archive/1859.1.3.tar.gz"
+sha256 "ac6167c6779dd7ae9e58a24d371367692f79ac624b2d27b653dcdedc416352db"
+# version "1859.1.3"
 
-depends_on "mulle-build" => :build
-depends_on "mulle-bootstrap" => :build
+depends_on "mulle-sde" => :build
 depends_on "cmake" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system "mulle-sde", "fetch"
+    system "xcodebuild", "-configuration", "Release", "DSTROOT=#{prefix}", "install"
   end
 
   test do
