73af7e2b14a8abc09229e0e9aa2c914a92a46663
Nat! <nat@mulle-kybernetik.com>
2023-01-13 20:40:52 +0100

    1859.1.3 1859.1.3 of mulle-scion

--- mulle-scion.rb
+++ mulle-scion.rb
@@ -2,11 +2,18 @@ class MulleScion < Formula
 desc "A modern template engine for Objective C"
 homepage "https://github.com/mulle-nat/MulleScion"
 url "https://github.com/mulle-nat/MulleScion/archive/1859.1.3.tar.gz"
-sha256 "ac6167c6779dd7ae9e58a24d371367692f79ac624b2d27b653dcdedc416352db"
+sha256 "68fb09b3a21bd153d6ec7b9a9bef095cf8042edbc8de16d14d710ed50fa79cd2"
 # version "1859.1.3"
 
-depends_on "mulle-sde" => :build
+depends_on "mulle-kybernetik/software/mulle-sde" => :build
+depends_on "mulle-kybernetik/software/mulle-env" => :build
+depends_on "mulle-kybernetik/software/mulle-platform" => :build
+depends_on "mulle-kybernetik/software/mulle-domain" => :build
+depends_on "mulle-kybernetik/software/mulle-sourcetree" => :build
+depends_on "mulle-kybernetik/software/mulle-bashfunctions" => :build
 depends_on "cmake" => :build
+depends_on "curl" => :build
+depends_on "git" => :build
 
   def install
     system "mulle-sde", "fetch"
