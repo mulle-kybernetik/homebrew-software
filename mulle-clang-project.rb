385719ad2e1591643aee81b523d21eb4a47f2713
Nat! <nat@mulle-kybernetik.com>
2024-01-11 18:25:16 +0100

    17.0.6.0 of mulle-clang-project

--- mulle-clang-project.rb
+++ mulle-clang-project.rb
@@ -1,51 +1,50 @@
 class MulleClangProject < Formula
-  desc "Objective-C compiler for the mulle-objc runtime"
+  desc "Objective-C compiler for the mulle-kybernetik runtime"
   homepage "https://github.com/mulle-cc/mulle-clang-project"
   license "BSD-3-Clause"
-  version "14.0.6.2"
+  version "17.0.6.0"
 #  revision 1
-  head "https://github.com/mulle-cc/mulle-clang-project.git", branch: "mulle/14.0.6"
+  head "https://github.com/mulle-cc/mulle-clang-project.git", branch: "mulle/17.0.6"
 
 #
 # MEMO:
-#    0. Replace 14.0.6.0 with x.0.0.0 your version number (and check vendor)
+#    0. Replace 17.0.6.0 with x.0.0.0 your version number (and check vendor)
 #    1. Create a release on github
 #    2. Download the tar.gz file from github like so
-#       `curl -O -L "https://github.com/mulle-cc/mulle-clang-project/archive/14.0.6.0.tar.gz"`
-#    3. Run shasum over it `shasum -a 256 -b 13.0.0.i1.tar.gz`
+#       `curl -O -L "https://github.com/mulle-cc/mulle-clang-project/archive/17.0.6.0.tar.gz"`
+#    3. Run shasum over it `shasum -a 256 -b 17.0.6.0.tar.gz`
 #    4. Remove bottle urls
 #
-  url "https://github.com/mulle-cc/mulle-clang-project/archive/refs/tags/14.0.6.2.tar.gz"
-  sha256 "299ebbdeafa4f5699f35b87b921f7222097d64f6b56d5e417633bf0419726833"
+  url "https://github.com/mulle-cc/mulle-clang-project/archive/refs/tags/17.0.6.0.tar.gz"
+  sha256 "abd71ef24387050ca4f22a6f8ea8f27a646b7979a0cc7dc9e4a17f6045fa96d9"
 
   def vendor
-    "mulle-clang 14.0.6.2 (runtime-load-version: 17)"
+    "mulle-clang 17.0.6.0 (runtime-load-version: 17)"
   end
 
 #
 # MEMO:
 #    For each OS X version, create bottles with:
 #
-#    `brew uninstall mulle-objc/software/mulle-clang-project`
+#    `brew uninstall mulle-kybernetik/software/mulle-clang-project`
 #    `brew install --formula --build-bottle mulle-clang-project.rb`
 # Now it gets retarded:
-#    `brew tap-new mulle-objc/software`
-#    `cp mulle-clang-project.rb /usr/local/Homebrew/Library/Taps/mulle-objc/homebrew-software/Formula/`
-#    `brew bottle mulle-objc/software/mulle-clang-project`
-#    `mv ./mulle-clang--14.0.6.0.monterey.bottle.tar.gz  ./mulle-clang-project-14.0.6.0.monterey.bottle.tar.gz`
+#    `brew tap-new mulle-kybernetik/software`
+#    `cp mulle-clang-project.rb /usr/local/Homebrew/Library/Taps/mulle-kybernetik/homebrew-software/Formula/`
+#    `brew bottle mulle-kybernetik/software/mulle-clang-project`
+#    `mv ./mulle-clang--17.0.6.0.sonoma.bottle.tar.gz  ./mulle-clang-project-17.0.6.0.sonoma.bottle.tar.gz`
 #
 #     scp -i ~/.ssh/id_rsa_hetzner_pw \
-#            ./mulle-clang-14.0.6.0.monterey.bottle.tar.gz \
+#            ./mulle-clang-17.0.6.0.sonoma.bottle.tar.gz \
 #            codeon@www262.your-server.de:public_html/_site/bottles/
 #
   bottle do
 #    "#{root_url}/#{name}-#{version}.#{tag}.bottle.#{revision}.tar.gz"
 #   root_url "https://www.mulle-kybernetik.com/bottles"
 
-    root_url "https://github.com/mulle-cc/mulle-clang-project/releases/download/14.0.6.2"
+    root_url "https://github.com/mulle-cc/mulle-clang-project/releases/download/17.0.6.0"
 
-    sha256 cellar: :any, monterey: "d9fe5d0177b19e74885bddf4ea4c9fcffd453a937ebeeee2116196e6b905cc54"
-    sha256 cellar: :any, big_sur: "c9f575ca3854f4b2cbeb6bf780a74f881356dbd02149f6af81a34f4f231f4907"
+    sha256 cellar: :any, sonoma: "aac00f815b7234abf66ac2bb868cc58630f7c2dce325ec351b07d5c5771f267a"
   end
 
 #
@@ -69,9 +68,15 @@ class MulleClangProject < Formula
   def install
     mkdir "build" do
       args = std_cmake_args
-      args << '-DLLVM_ENABLE_PROJECTS=libcxxabi;libcxx;clang'
+      args << '-DLLVM_BUILD_LLVM_DYLIB=ON'
+      args << "-DLLVM_ENABLE_PROJECTS='clang'" # ";compiler-rt'" don't build for now on sonoma
+      args << "-DLLVM_ENABLE_RUNTIMES='libcxxabi;libcxx'"
+      args << '-DLLVM_LINK_LLVM_DYLIB=ON'
+      args << '-DLLVM_PARALLEL_LINK_JOBS=4'
       args << '-DCMAKE_BUILD_TYPE=Release'
       args << '-DCLANG_VENDOR=mulle' 
+      args << "-DCMAKE_SHARED_LINKER_FLAGHS=''-Wl,--reduce-memory-overheads'"
+      args << "-DCMAKE_EXE_LINKER_FLAGHS='-Wl,--reduce-memory-overheads'"
       args << '-DCMAKE_INSTALL_MESSAGE=LAZY'
       args << "-DCMAKE_INSTALL_PREFIX='#{prefix}/root'"
       args << '../llvm'
@@ -80,6 +85,7 @@ class MulleClangProject < Formula
       system "ninja", "install"
     end
     bin.install_symlink "#{prefix}/root/bin/clang" => "mulle-clang"
+    bin.install_symlink "#{prefix}/root/bin/nm" => "mulle-nm"
     bin.install_symlink "#{prefix}/root/bin/scan-build" => "mulle-scan-build"
   end
 
@@ -87,7 +93,7 @@ class MulleClangProject < Formula
     str = <<~EOS
     To use mulle-clang inside homebrew formulae, you need a shim.
     See:
-       https://github.com/mulle-objc/mulle-clang-homebrew
+       https://github.com/mulle-kybernetik/mulle-clang-homebrew
     EOS
     str
   end
