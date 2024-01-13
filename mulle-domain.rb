55769815924262903472363d3af5b803484536dc
Nat! <nat@mulle-kybernetik.com>
2024-01-13 21:01:20 +0100

    1.4.0 1.4.0 of mulle-domain

--- mulle-domain.rb
+++ mulle-domain.rb
@@ -2,7 +2,7 @@ class MulleDomain < Formula
 desc "🏰 URL management and tag resolution for repositories"
 homepage "https://github.com/mulle-sde/mulle-domain"
 url "https://github.com/mulle-sde/mulle-domain/archive/1.4.0.tar.gz"
-sha256 "c6225db07b3f9c8f8b0de3af54154cbabc9afbb8ff146bc5cd649f5fce0918db"
+sha256 "92df57afb4f8e42d459ed97adfd25cf5414a0642d84673de8b7353744a2ed1b2"
 # version "1.4.0"
 
 depends_on "mulle-kybernetik/software/mulle-semver"
