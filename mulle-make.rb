9f632c7a67e947a6799fdd613cbc106cd49e20b5
Nat! <nat@mulle-kybernetik.com>
2024-01-15 23:58:02 +0100

    2.2.0 2.2.0 of mulle-make

--- mulle-make.rb
+++ mulle-make.rb
@@ -2,7 +2,7 @@ class MulleMake < Formula
 desc "💄 Build projects with tools like cmake, meson, autoconf"
 homepage "https://github.com/mulle-sde/mulle-make"
 url "https://github.com/mulle-sde/mulle-make/archive/2.2.0.tar.gz"
-sha256 "6578280f717f182ea5e08edcd991e00c206de670f13ee637ea4b4f0c6445460c"
+sha256 "22f7be37620d38e8416b0ff509bd1d8ab96ba8a1fcb86d693856527b5948012d"
 # version "2.2.0"
 
 depends_on "mulle-kybernetik/software/mulle-env"
