1b1024d77296ba995ae01e625b2fae631efc01e9
Nat! <nat@mulle-kybernetik.com>
2024-01-16 00:03:25 +0100

    3.2.1 3.2.1 of mulle-dispense

--- mulle-dispense.rb
+++ mulle-dispense.rb
@@ -2,7 +2,7 @@ class MulleDispense < Formula
 desc " 🚰 Copy build products and reorganize them "
 homepage "https://github.com/mulle-sde/mulle-dispense"
 url "https://github.com/mulle-sde/mulle-dispense/archive/3.2.1.tar.gz"
-sha256 "e447f9204b37f4fb4805471ec40f6ef496b0346925b3cdb9e4714584f31538ff"
+sha256 "487e00f698f28579de4a097b717f75cdc1169fd6446f853690985e0189294b74"
 # version "3.2.1"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
