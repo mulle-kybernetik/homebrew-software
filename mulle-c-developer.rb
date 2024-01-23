9130277dc4baf00a36f014e0e292fcaaf26ac601
Nat! <nat@mulle-kybernetik.com>
2024-01-23 02:27:57 +0100

    0.17.1 0.17.1 of mulle-c-developer

--- mulle-c-developer.rb
+++ mulle-c-developer.rb
@@ -1,9 +1,9 @@
 class MulleCDeveloper < Formula
 desc "🧢 mulle-c developer kit for mulle-sde"
 homepage "https://github.com/mulle-c/mulle-c-developer"
-url "https://github.com/mulle-c/mulle-c-developer/archive/0.17.0.tar.gz"
-sha256 "51da8241c38d041a42b6b76425b9af35b2119cb5cd76fccd8867f7f47720e2aa"
-# version "0.17.0"
+url "https://github.com/mulle-c/mulle-c-developer/archive/0.17.1.tar.gz"
+sha256 "ca80fc52b83faf0f6ee62b1c411b35ded3c55eb931a427ffee2af0729762428a"
+# version "0.17.1"
 
 depends_on "mulle-kybernetik/software/mulle-sde-developer"
 depends_on "mulle-kybernetik/software/mulle-test"
