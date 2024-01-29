0234b49dfedfd494608d778d98ac9505291b055f
Nat! <nat@mulle-kybernetik.com>
2024-01-29 18:46:51 +0100

    3.2.2 3.2.2 of mulle-craft

--- mulle-craft.rb
+++ mulle-craft.rb
@@ -1,9 +1,9 @@
 class MulleCraft < Formula
 desc "🚬 Build projects using mulle-make and mulle-sourcetree"
 homepage "https://github.com/mulle-sde/mulle-craft"
-url "https://github.com/mulle-sde/mulle-craft/archive/3.2.1.tar.gz"
-sha256 "45ea4be0bdee3731a229ca78d8cb3335d84e8a039c52a87b4fef54360f28063a"
-# version "3.2.1"
+url "https://github.com/mulle-sde/mulle-craft/archive/3.2.2.tar.gz"
+sha256 "869348e5b752bd6021051a130e8da7a2f9bb5e808b49b49cb34f7c4ffc34de5f"
+# version "3.2.2"
 
 depends_on "mulle-kybernetik/software/mulle-sourcetree"
 depends_on "mulle-kybernetik/software/mulle-make"
