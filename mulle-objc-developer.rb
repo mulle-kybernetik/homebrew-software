19b69cec1050b3b7aaa1d7dfff2a09673cc4926f
Nat! <nat@mulle-kybernetik.com>
2024-02-01 01:08:01 +0100

    handpatched compiler into formula

--- mulle-objc-developer.rb
+++ mulle-objc-developer.rb
@@ -6,6 +6,8 @@ sha256 "a14e0d7c2d354d1ffdbc36a51c314c546405f6c132d1764047ac3a7fcdca5c62"
 # version "0.26.1"
 
 depends_on "mulle-kybernetik/software/mulle-c-developer"
+depends_on "mulle-kybernetik/software/mulle-clang-project"
+
 def install
   system "./bin/installer", "#{prefix}"
 end
