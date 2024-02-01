51d0723fda41ee9bf8f05077c25d2430eb3e456f
Nat! <nat@mulle-kybernetik.com>
2024-02-01 14:12:28 +0100

    0.1.1 0.1.1 of mulle-menu

--- mulle-menu.rb
+++ mulle-menu.rb
@@ -1,9 +1,9 @@
 class MulleMenu < Formula
 desc "🍟 A menu for your bash scripts"
 homepage "https://github.com/mulle-sde/mulle-menu"
-url "https://github.com/mulle-sde/mulle-menu/archive/0.1.0.tar.gz"
-sha256 "7abddb8beb5a7717f8e9fc8c60ba89df42490ee354cdea5a14e66c0bded38de4"
-# version "0.1.0"
+url "https://github.com/mulle-sde/mulle-menu/archive/0.1.1.tar.gz"
+sha256 "452b5faf7bbc06ec973ab04f64cda57023cf7a9eb595524d468a6697782058b5"
+# version "0.1.1"
 def install
   system "./bin/installer", "#{prefix}"
 end
