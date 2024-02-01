98f8de29489a448cca0e30fee68a1ccc52c3f7bb
Nat! <nat@mulle-kybernetik.com>
2024-02-01 18:35:31 +0100

    fix typo

--- mulle-clang-project.rb
+++ mulle-clang-project.rb
@@ -79,7 +79,7 @@ class MulleClangProject < Formula
       args << '-DCMAKE_BUILD_TYPE=Release'
       args << '-DCLANG_VENDOR=mulle' 
       args << "-DLLVM_TARGETS_TO_BUILD='X86;ARM;AArch64'" \
-      args << "-DCMAKE_SHARED_LINKER_FLAGS=''-Wl,--reduce-memory-overheads'"
+      args << "-DCMAKE_SHARED_LINKER_FLAGS='-Wl,--reduce-memory-overheads'"
       args << "-DCMAKE_EXE_LINKER_FLAGS='-Wl,--reduce-memory-overheads'"
       args << '-DCMAKE_INSTALL_MESSAGE=LAZY'
       args << "-DCMAKE_INSTALL_PREFIX='#{prefix}/root'"
