791dd8ed59a381defbc8193b6c62d9aa31fb3d3d
Nat! <nat@mulle-kybernetik.com>
2024-11-20 18:19:02 +0100

    6.4.0 6.4.0 of mulle-test

--- mulle-test.rb
+++ mulle-test.rb
@@ -1,9 +1,9 @@
 class MulleTest < Formula
 desc "☑︎ Run tests and compare their results against expectations"
 homepage "https://github.com/mulle-sde/mulle-test"
-url "https://github.com/mulle-sde/mulle-test/archive/6.3.3.tar.gz"
-sha256 "5a7fa7ee6eb2cdd7d3adfc65e0bc4745d85808e6f7a877624439da319039f1eb"
-# version "6.3.3"
+url "https://github.com/mulle-sde/mulle-test/archive/6.4.0.tar.gz"
+sha256 "29b724996e790fee0edcfa43c090b30b3a5e2fe54f05830d904dc9c564ebc3e6"
+# version "6.4.0"
 
 depends_on "mulle-kybernetik/software/mulle-platform"
 def install
