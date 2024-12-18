9ff5e15664466bec0e50cdeb1e52f2a5175f4be6
Nat! <nat@mulle-kybernetik.com>
2024-12-18 18:06:45 +0100

    18.48.7 18.48.7 of mulle-genstrings

--- mulle-genstrings.rb
+++ mulle-genstrings.rb
@@ -1,9 +1,9 @@
 class MulleGenstrings < Formula
 desc "🗻 Create Localizable.strings from NSLocalizedString"
 homepage "https://github.com/mulle-cc/mulle-genstrings"
-url "https://github.com/mulle-cc/mulle-genstrings/archive/18.48.6.tar.gz"
-sha256 "b1c490f56e87d2c0caac825976bcda64a84bbe5059760a02feec24836e7a0f96"
-# version "18.48.6"
+url "https://github.com/mulle-cc/mulle-genstrings/archive/18.48.7.tar.gz"
+sha256 "9ea8eadc14da84d85d87c4b67eec386d1a68ac2ff15e0c4a26a08392959bf0c1"
+# version "18.48.7"
   depends_on :xcode => :build
   depends_on :macos
 
