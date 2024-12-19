38509bfc892efbd0ea1bbe6841d448be51d00b39
Nat! <nat@mulle-kybernetik.com>
2024-12-20 00:21:21 +0100

    4.1.2 4.1.2 of mulle-project

--- mulle-project.rb
+++ mulle-project.rb
@@ -2,7 +2,7 @@ class MulleProject < Formula
 desc "🤷🏾‍♀️ Manage project versions and releases"
 homepage "https://github.com/mulle-nat/mulle-project"
 url "https://github.com/mulle-nat/mulle-project/archive/4.1.2.tar.gz"
-sha256 "2a098964c866fc620a8ce9adb07e3320bc6e4e16dd282f503517e1735fed726a"
+sha256 "b91d51cb674c8cc19d8d70f37425705bb0805d5f65126d5587df5d36c99f01db"
 # version "4.1.2"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
