53d1b62eb2924a22914780c812f77d418ad21742
Nat! <nat@mulle-kybernetik.com>
2025-01-09 12:36:42 +0100

    5.3.2 5.3.2 of mulle-env

--- mulle-env.rb
+++ mulle-env.rb
@@ -1,9 +1,9 @@
 class MulleEnv < Formula
 desc "🌳 Virtual environment for C and Objective-C"
 homepage "https://github.com/mulle-sde/mulle-env"
-url "https://github.com/mulle-sde/mulle-env/archive/5.3.1.tar.gz"
-sha256 "a028521555ec6d41d98814769e3a1bc52120af5778486e4326db309a8252272a"
-# version "5.3.1"
+url "https://github.com/mulle-sde/mulle-env/archive/5.3.2.tar.gz"
+sha256 "ec26dfd5890ca07e28da1c571588d26452eb178be5ba06c8d4e92cc1068fcc5e"
+# version "5.3.2"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
