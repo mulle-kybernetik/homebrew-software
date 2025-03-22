759d349df7d1ad32d6f0b99dfb195cb3bf1b3575
Nat! <nat@mulle-kybernetik.com>
2025-03-22 10:54:43 +0100

    1.0.6 1.0.6 of mulle-semver

--- mulle-semver.rb
+++ mulle-semver.rb
@@ -1,9 +1,9 @@
 class MulleSemver < Formula
 desc "📍 semantic versioning tool"
 homepage "https://github.com/mulle-sde/mulle-semver"
-url "https://github.com/mulle-sde/mulle-semver/archive/1.0.5.tar.gz"
-sha256 "cc41477407c06bee5955235c8ee10e2779d014e9d4f896e94f008252b467e7cf"
-# version "1.0.5"
+url "https://github.com/mulle-sde/mulle-semver/archive/1.0.6.tar.gz"
+sha256 "731a72493d17e3fd06a791c6854091c52d4b01af87cf47ec7847415f2a91f639"
+# version "1.0.6"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
