c16d7e5646eb5cd1418552e1e9b0eb2fddf1d21f
Nat! <nat@mulle-kybernetik.com>
2026-01-20 20:49:28 +0100

    7.1.0 7.1.0 of mulle-test

--- mulle-test.rb
+++ mulle-test.rb
@@ -2,7 +2,7 @@ class MulleTest < Formula
 desc "☑︎ Run tests and compare their results against expectations"
 homepage "https://github.com/mulle-sde/mulle-test"
 url "https://github.com/mulle-sde/mulle-test/archive/7.1.0.tar.gz"
-sha256 "83769f3c60a42e2a96cc2f81ead0d7e44a5037b9ee17d30adf1b306985d94f4e"
+sha256 "8da29b64bd55991eb6b965396a7286151e88afd2acd3a02e1c71579df8b03cab"
 # version "7.1.0"
 
 depends_on "mulle-kybernetik/software/mulle-platform"
