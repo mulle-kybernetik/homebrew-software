2748874c74257181fb5fdb55751c74da9717b474
Nat! <nat@mulle-kybernetik.com>
2026-01-20 16:02:22 +0100

    1.6.3 1.6.3 of mulle-sourcetree

--- mulle-sourcetree.rb
+++ mulle-sourcetree.rb
@@ -1,9 +1,9 @@
 class MulleSourcetree < Formula
 desc "🌲 Project composition and maintenance with build support"
 homepage "https://github.com/mulle-sde/mulle-sourcetree"
-url "https://github.com/mulle-sde/mulle-sourcetree/archive/1.6.2.tar.gz"
-sha256 "0058c913163a8844d92b5e405272d30dfd094ae0faa075ad1578203f6a2523de"
-# version "1.6.2"
+url "https://github.com/mulle-sde/mulle-sourcetree/archive/1.6.3.tar.gz"
+sha256 "ba3a02c4b9f9ffa74c2c7c8ea1dc96b35ea10075acdd1f94d09bac5a8a416a8c"
+# version "1.6.3"
 
 depends_on "mulle-kybernetik/software/mulle-fetch"
 depends_on "mulle-kybernetik/software/mulle-domain"
