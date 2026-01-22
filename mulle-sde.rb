beb6523ffd7a0ba2213b4a79bbd5a2556664a881
Nat! <nat@mulle-kybernetik.com>
2026-01-22 19:44:32 +0100

    3.6.0 3.6.0 of mulle-sde

--- mulle-sde.rb
+++ mulle-sde.rb
@@ -1,9 +1,9 @@
 class MulleSde < Formula
 desc "💠 Cross-platform IDE for the command-line"
 homepage "https://github.com/mulle-sde/mulle-sde"
-url "https://github.com/mulle-sde/mulle-sde/archive/3.5.2.tar.gz"
-sha256 "b662743df1e81137a39e59a8f97c26695a5db4fd61577ffd4f3ebdfa0733c103"
-# version "3.5.2"
+url "https://github.com/mulle-sde/mulle-sde/archive/3.6.0.tar.gz"
+sha256 "649b482d6b8d538a4bf75a60b10077f6827a8603db0d9511252ca68e52a363d4"
+# version "3.6.0"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 depends_on "mulle-kybernetik/software/mulle-env"
