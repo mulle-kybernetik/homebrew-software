70e842eb141a275b173506d324178ffdb5d4bdcf
Nat! <nat@mulle-kybernetik.com>
2026-03-19 12:07:15 +0100

    0.2.3 0.2.3 of mulle-todo

--- mulle-todo.rb
+++ mulle-todo.rb
@@ -1,9 +1,9 @@
 class MulleTodo < Formula
 desc "📋 A Todo list manager for shell environment"
 homepage "https://github.com/mulle-sde/mulle-todo"
-url "https://github.com/mulle-sde/mulle-todo/archive/0.2.2.tar.gz"
-sha256 "e21863479ffa42680936500eb3ebc23051ced24ab7f5171cc6413b417035c426"
-# version "0.2.2"
+url "https://github.com/mulle-sde/mulle-todo/archive/0.2.3.tar.gz"
+sha256 "c4822c91ff96cda4f3433d0c7d0e12607aa7932b1e97ba13e3cc2422a97b5432"
+# version "0.2.3"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
