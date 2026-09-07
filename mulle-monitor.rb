740f64f179bb7b0535193134dae54f7cdc61bdd3
Nat! <nat@mulle-kybernetik.com>
2026-09-07 23:47:15 +0200

    1.1.1 1.1.1 of mulle-monitor

--- mulle-monitor.rb
+++ mulle-monitor.rb
@@ -1,9 +1,9 @@
 class MulleMonitor < Formula
 desc "🕵🏻‍ Extensible filesystem observation with callbacks"
 homepage "https://github.com/mulle-sde/mulle-monitor"
-url "https://github.com/mulle-sde/mulle-monitor/archive/1.1.0.tar.gz"
-sha256 "154cdf21c637bf471ff89e8fd11cc9e83a72d6d0626a00ecc2396a0648778a1a"
-# version "1.1.0"
+url "https://github.com/mulle-sde/mulle-monitor/archive/1.1.1.tar.gz"
+sha256 "b27c3cd0a2c51e485c5ecb563aa0583eb701a26c3d10a8e598e844857057f6b6"
+# version "1.1.1"
 
 depends_on "mulle-kybernetik/software/mulle-match"
 def install
