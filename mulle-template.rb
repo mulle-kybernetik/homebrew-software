e7fdaa213733b69a77b91dfcce2c947f26158bb4
Nat! <nat@mulle-kybernetik.com>
2026-09-07 23:49:24 +0200

    1.2.1 1.2.1 of mulle-template

--- mulle-template.rb
+++ mulle-template.rb
@@ -1,9 +1,9 @@
 class MulleTemplate < Formula
 desc "🕋 Generate files from templates"
 homepage "https://github.com/mulle-sde/mulle-template"
-url "https://github.com/mulle-sde/mulle-template/archive/1.2.0.tar.gz"
-sha256 "b5f73b0b51a8436803d76c6183af368f07b0390959da0fbabf7defd83f6e0933"
-# version "1.2.0"
+url "https://github.com/mulle-sde/mulle-template/archive/1.2.1.tar.gz"
+sha256 "05143fee74551b1aa4d962e7618857b132f3a5a5646d3f56f41e5375ed0ab497"
+# version "1.2.1"
 
 depends_on "mulle-kybernetik/software/mulle-bashfunctions"
 def install
